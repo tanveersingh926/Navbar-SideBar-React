463783aa1defccef32c1f62e234af0f7eef8ea3b
tanveersingh926 <tanveer.singh926@gmail.com>
2018-07-09 12:55:13 +0800

    Added backdrop as component

--- src/App.js
+++ src/App.js
@@ -1,8 +1,9 @@
 import React, { Component } from 'react';
 // import logo from './logo.svg';
 
-import  Toolbar  from "./components/Toolbar/Toolbar";
+import Toolbar  from "./components/Toolbar/Toolbar";
 import SideDrawer from "./components/SideDrawer/SideDrawer"
+import Backdrop from './components/Backdrop/Backdrop';
 
 class App extends Component {
   render() {
@@ -10,6 +11,7 @@ class App extends Component {
       <div style={{height:'100%'}}>
         <Toolbar/>
         <SideDrawer />
+        <Backdrop />
         <main style={{marginTop:'64px'}}>
           <p>This is a page content</p>
         </main>
