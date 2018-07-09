3bcbe3955f1d34ecbd503071a9fe6137e47c92de
tanveersingh926 <tanveer.singh926@gmail.com>
2018-07-09 11:34:46 +0800

    Added required CSS for toolbar

--- src/App.js
+++ src/App.js
@@ -1,12 +1,16 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
-import './App.css';
+
+import  Toolbar  from "./components/Toolbar/Toolbar";
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        
+        <Toolbar/>
+        <main style={{marginTop:'64px'}}>
+          <p>This is a page content</p>
+        </main>
       </div>
     );
   }
--- src/components/Toolbar/Toolbar.js
+++ src/components/Toolbar/Toolbar.js
@@ -1,11 +1,12 @@
 import React from 'react';
+import './Toolbar.css'
 
 const toolbar = props => (
-    <header>
-        <nav>
+    <header className="toolbar">
+        <nav className="toolbar__navigation">
             <div></div>
-            <div><a href="/">The Logo</a></div>
-            <div>
+            <div className="toolbar__logo"><a href="/">The Logo</a></div>
+            <div  className="toolbar_navigation-items">
                 <ul>
                     <li><a href="/">Products</a></li>
                     <li><a href="/">Users</a></li>
