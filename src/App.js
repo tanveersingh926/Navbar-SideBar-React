2d38b71a0d3186e77e1230c5521afd5c1fb58882
tanveersingh926 <tanveer.singh926@gmail.com>
2018-07-09 12:44:55 +0800

    completed Side Drawer component base css

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import React, { Component } from 'react';
 // import logo from './logo.svg';
 
 import  Toolbar  from "./components/Toolbar/Toolbar";
+import SideDrawer from "./components/SideDrawer/SideDrawer"
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
+      <div style={{height:'100%'}}>
         <Toolbar/>
+        <SideDrawer />
         <main style={{marginTop:'64px'}}>
           <p>This is a page content</p>
         </main>
